4d9a1146ee0b616be4b93c1b8c2ff331a94f5f6b
Kavya Mukhija <mukhijakavya02@gmail.com>
2025-02-11 13:19:58 +0530

    yo

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 let img = document.querySelector(".meme img");
-        async function randomMemeGenerator(){
-            let res = await fetch("https://meme-api.com/gimme");    
-            console.log(res);
-            let json = await res.json();
-            console.log(json);
-            let url = json.url;
-            console.log(url);
-            img.src = url;
+let generateBtn = document.querySelector(".generate-btn");
 
-        }
-        randomMemeGenerator();
-        
-        let timer = setInterval(randomMemeGenerator,2000);
+async function randomMemeGenerator(){
+    let res = await fetch("https://meme-api.com/gimme");    
+    console.log(res);
+    let json = await res.json();
+    console.log(json);
+    let url = json.url;
+    console.log(url);
+    img.src = url;
+}
 
-        img.addEventListener("mouseover",function(){
-            clearInterval(timer);
-        })
+randomMemeGenerator();
 
-        img.addEventListener("mouseleave",function(){
-            timer = setInterval(randomMemeGenerator,2000);
-        })
+let timer = setInterval(randomMemeGenerator, 7000);
+
+img.addEventListener("mouseover", function(){
+    clearInterval(timer);
+});
+
+img.addEventListener("mouseleave", function(){
+    timer = setInterval(randomMemeGenerator, 7000);
+});
+
+generateBtn.addEventListener("click", function() {
+    randomMemeGenerator();
+    // Reset the timer to avoid immediate auto-refresh after button click
+    clearInterval(timer);
+    timer = setInterval(randomMemeGenerator, 7000);
+});
